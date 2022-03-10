459648b7dd8c22dab9fec86657668622d63ac33b
ZHJ0125 <shandonghoujin@163.com>
2022-03-10 19:09:58 +0800

    Complete the front-end framework

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, url_for, redirect
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ZHJ0125-bushu-dev'
 
 name = 'ZHJ0125'
 movies = [
@@ -15,8 +17,17 @@ movies = [
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        flash('抱歉兄弟，后端代码还没写呢，敬请期待😂')  # 显示错误提示
+        # 获取表单数据
+        username = request.form.get('username')  # 传入表单对应输入字段的 name 值
+        password = request.form.get('password')
+        steps = request.form.get('steps')
+        if not username or not password or not steps:
+            flash('抱歉兄弟，后端代码还没写呢，敬请期待😂')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
     return render_template('index.html', name=name, movies=movies)
 
 @app.errorhandler(404)  # 传入要处理的错误代码
