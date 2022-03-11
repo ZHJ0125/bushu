784dbc60827b16e92b073a291eb3991a905dd6e3
ZHJ0125 <shandonghoujin@163.com>
2022-03-11 17:07:55 +0800

    Forms replaced by the Flask-WTF framework

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from time import sleep
+from tkinter import DISABLED
 from flask import Flask, render_template, request, flash, url_for, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Regexp
 
 app = Flask(__name__)
@@ -9,31 +11,34 @@ app.config['SECRET_KEY'] = 'ZHJ0125-bushu-dev'
 name = 'ZHJ0125'
 
 class MyForm(FlaskForm):
-    mobile = StringField('手机', validators=[DataRequired(), Regexp('^1[35789]\d{9}$', 0, '手机号码不合法')], render_kw={'placeholder': '输入手机号'})
+    username = StringField('小米账号（手机号）', validators=[DataRequired(), Regexp('^1[35789]\d{9}$')], render_kw={'type':'number', 'class':"input", 'placeholder':"a"})
+    password = StringField('密码', validators=[DataRequired()], render_kw={'type':'password', 'class':"input", 'placeholder':"a"})
+    steps = StringField('步数（建议2000~40000）', validators=[DataRequired()], render_kw={'type':'number', 'class':"input", 'placeholder':"a"})
+    submit = SubmitField('提交', render_kw={'type':"submit", 'class':"submitBtn"})
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        flash('抱歉兄弟，后端代码还没写呢，敬请期待😂')  # 显示错误提示
-        # 获取表单数据
-        username = request.form.get('username')  # 传入表单对应输入字段的 name 值
-        password = request.form.get('password')
-        steps = request.form.get('steps')
-        print("[Debug] -> username="+username, "password="+password, "steps="+steps)
-        if not username or not password or not steps:
-            flash('输入的数据有误')  # 显示错误提示
-            return redirect(url_for('index'))  # 重定向回主页
-    # 默认GET请求，返回主页面
-    return render_template('index.html', name=name)
-
-@app.route('/test', methods=['GET', 'POST'])
-def test():
     form = MyForm()
-    if form.validate_on_submit(): # 此方法会自动验证POST方式以及数据格式是否合法
-        print("mobile = " + form.mobile.data)
-    else:
-        print("手机格式有误")
-    return render_template('test.html', form=form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            # 数据格式正确，获取表单数据
+            print("username = " + form.username.data)
+            print("password = " + form.password.data)
+            print("steps = " + form.steps.data)
+            if(int(form.steps.data) <= 0 or int(form.steps.data) > 40000):
+                flash('步数超出范围，请重新输入步数', 'error')
+                return render_template('index.html', form=form)
+            flash('正在请求刷新步数，请稍等...')
+            form.submit.render_kw = {'type':"submit", 'class':"submitBtn_disable", 'disabled': 'disabled'}  # 禁用submit
+            
+            sleep(5)  # 此处用来留作API请求
+            
+            form.submit.render_kw = {'type':"submit", 'class':"submitBtn"}  # 启用submit
+        else:
+            flash('输入的数据格式有误，请重新输入账号和密码', 'error')  # 显示错误提示
+            return render_template('index.html', form=form)
+    # 默认GET请求，返回主页面
+    return render_template('index.html', form=form)
 
 @app.errorhandler(404)  # 传入要处理的错误代码
 def page_not_found(e):  # 接受异常对象作为参数
